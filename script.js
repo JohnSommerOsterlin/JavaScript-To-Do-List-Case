d1dc6fbb9b5eb7a14af1bb739a008eeb950c42b9
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2021-11-25 14:41:09 +0100

    added new files

--- script.js
+++ script.js
@@ -1,1 +1,75 @@
-console.log("testing");
+function newElement () {
+    let li = document.createElement("li");
+    let text = document.getElementById("input-field").value;
+    console.log(li);
+
+    li.append(text);
+    document.body.appendChild(li);
+    console.log(text);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let li = document.createElement ("li");
+//     let text = document.getElementById("input-field").value;
+//     console.log();
+
+//     li.append(text);
+//     document.body.appendChild(li);
+//     console.log(li);
