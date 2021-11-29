a1e3cbce710356b42926d401a4811a209ad47ef4
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2021-11-29 14:32:12 +0100

    added new functions

--- script.js
+++ script.js
@@ -1,75 +1,38 @@
-function newElement () {
-    let li = document.createElement("li");
-    let text = document.getElementById("input-field").value;
-    console.log(li);
+function newElement() {
+  let li = document.createElement("li");
+  let inputField = document.getElementById("inputField").value;
+  let removeButton = document.createElement("button");
+  removeButton.className = "closeLi";
 
-    li.append(text);
-    document.body.appendChild(li);
-    console.log(text);
+  li.append(inputField);
+  ulList.appendChild(li);
+  li.appendChild(removeButton)
+  removeButton.innerHTML = "&#10005";
+
+
+
+  // li.addEventListener("click", function () {
+  //   // let check = document.createElement("span")
+  //   // check.innerHTML = "&#10004";
+  //   // li.append(check)
+  //   // li.style.textDecoration = "line-through";
+  //   li.style.boxShadow = "inset -3px -3px 7px #ffffff73, inset 3px 3px 5px rgba(94, 104, 121, .288)"
+  // })
+
+  // let button = document.getElementsByClassName("closeLi") 
+  // button.addEventListener("click", function () {
+  // li.remove();
+  // })
 }
 
+let ulList = document.getElementById("ulList");
+console.log(ulList);
+ulList.addEventListener("click", function (event) {
+  
+  if (event.target.nodeName === "BUTTON") {
+    event.target.parentElement.remove();
+  } else {
+    event.target.classList.toggle("highlight");
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let li = document.createElement ("li");
-//     let text = document.getElementById("input-field").value;
-//     console.log();
-
-//     li.append(text);
-//     document.body.appendChild(li);
-//     console.log(li);
+  }
+})
