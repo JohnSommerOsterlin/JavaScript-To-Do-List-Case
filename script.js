0d71b25d2f182465f540637b6179cec65e97d430
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2021-11-30 11:39:19 +0100

    added new elements

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 function newElement() {
+  let checkMark = document.createElement("SPAN");
+  checkMark.innerHTML = "&#10004";
   let li = document.createElement("li");
   let inputField = document.getElementById("inputField").value;
   let removeButton = document.createElement("button");
   removeButton.className = "closeLi";
 
+  li.append(checkMark);
   li.append(inputField);
   ulList.appendChild(li);
   li.appendChild(removeButton)
   removeButton.innerHTML = "&#10005";
 
-
-
-  // li.addEventListener("click", function () {
-  //   // let check = document.createElement("span")
-  //   // check.innerHTML = "&#10004";
-  //   // li.append(check)
-  //   // li.style.textDecoration = "line-through";
-  //   li.style.boxShadow = "inset -3px -3px 7px #ffffff73, inset 3px 3px 5px rgba(94, 104, 121, .288)"
-  // })
-
-  // let button = document.getElementsByClassName("closeLi") 
-  // button.addEventListener("click", function () {
-  // li.remove();
-  // })
 }
 
 let ulList = document.getElementById("ulList");
@@ -32,7 +21,8 @@ ulList.addEventListener("click", function (event) {
   if (event.target.nodeName === "BUTTON") {
     event.target.parentElement.remove();
   } else {
+    event.target.firstElementChild.classList.toggle("checkMarkSpan");
     event.target.classList.toggle("highlight");
-
-  }
+    console.log(event.target);
+  } 
 })
