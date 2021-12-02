9fadca4ad97e027989de8f33b3a8edf01bdaa4c5
JohnSommerOsterlin <90193135+JohnSommerOsterlin@users.noreply.github.com>
2021-12-02 17:21:27 +0100

    added new elements

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 function newElement() {
-  let checkMark = document.createElement("SPAN");
-  checkMark.innerHTML = "&#10004";
+  // CREATING LIST ITEM
   let li = document.createElement("li");
   let inputField = document.getElementById("inputField").value;
+
+  // CREATING SPAN ELEMENT
+  let checkMark = document.createElement("SPAN");
+  checkMark.innerHTML = "&#10004";
+
+  // CREATING REMOVE BUTTON
   let removeButton = document.createElement("button");
-  let inputFieldReset = document.getElementsByClassName("inputFieldContainer")
   removeButton.className = "closeLi";
 
+
+  // INSERT LIST ITEM, CHECKMARK & REMOVE BUTTON INTO DOM IF INPUT FIELD IS NOT EMPTY
+  if (document.getElementById("inputField").value != "") {
   li.append(checkMark);
   li.append(inputField);
   ulList.appendChild(li);
   li.appendChild(removeButton)
-  removeButton.innerHTML = "&#10005";
-  console.log(inputFieldReset);
-
+  removeButton.innerHTML = "&#10005"; 
 }
 
+  // CLEARING INPUT FIELD
+  document.getElementById("inputField").value = "";
+}
+
+
+// UNORDERED LIST
 let ulList = document.getElementById("ulList");
-console.log(ulList);
+
+// MARKED COMPLETE & REMOVE LIST ITEM
 ulList.addEventListener("click", function (event) {
   
+  // REMOVE LIST ITEM
   if (event.target.nodeName === "BUTTON") {
     event.target.parentElement.remove();
+
+  // MARKED COMPLETE LIST ITEM
   } else {
     event.target.firstElementChild.classList.toggle("checkMarkSpan");
     event.target.classList.toggle("highlight");
